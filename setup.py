2c93b90cb179d9b7d819e9689e121c104c9ab2f0
Maulik Nariya <mauliknariya@gmail.com>
2020-06-26 17:16:36 -0400

    setup file

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ VERSION = versioneer.get_version()
 CMDCLASS = versioneer.get_cmdclass()
 DESCRIPTION = 'Leave-pair-out cross-validation'
 LONG_DESCRIPTION = '''
-***lpocv*** is a Python module that implements leave-pair-out cross-validation 
+**lpocv** is a Python module that implements leave-pair-out cross-validation 
 for machine learning classification and regression problems. It allows the user 
 to match the samples by a confounding batch variable and also include errors 
 on the labels (for regression problems).
