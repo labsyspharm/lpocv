42d3a3cf868aaaa01ec30b3f29fe8475f4c3f822
Maulik Nariya <mauliknariya@gmail.com>
2020-07-17 13:09:46 -0400

    closest match and survival prediction

--- lpocv/lpocv.py
+++ lpocv/lpocv.py
@@ -1,122 +1,185 @@
 import numpy as np
-from numpy.random import choice
+from numpy.random import choice, randint, RandomState
 from sklearn.utils.validation import indexable, _num_samples
 from itertools import combinations
 
 class LeavePairOut:
     '''
-    Splits the data into train-test by leaving 2 samples out in each CV-fold with an option of
-    returning only those test pairs with matching group labels
-    '''
-    def split(self, X, y, erry=None, groups=None, match_groups=False, 
-             match_window=None, orderN=False, random_state=None):
-        '''
+    Splits the data into train-test by choosing a pair of rankable samples as test 
+    pair with several options such as O(N) pairs, group (or batch) matched pairs.
+    
+    Attributes:
+    -----------
+    split: generates the train-test split as per the specified conditions. 
         Parameters:
         -----------
         X: array-like, shape=(n_samples, n_features)
             data matrix
         y: array-like, shape=(n_samples)
-            class labels
+            labels, allowed types: str, int, float, (bool, int)
         erry: float or array-like
             error in y for regression problems, must be non-negative real number(s)
-            will return test pairs for which 
-                abs(y[i]-y[j]) >= erry, if erry is scalar) 
-                abs(y[i]-y[j]) >= max(erry[i], erry[j]), if  erry is array-like
         groups: array-like, shape=(n_samples)
             group labels
-        match_groups: bool, default=False
-            if True then returns train test split such that test pairs have the same 
-            group labels
-        match_window: float, default=None
-            the allowed window for matching the groups when the groups are 
-            float values
-            if match_groups is True and match_window is specified, it will return 
-            entries for which  abs(groups[i]-groups[j]) < match_window)
+        match_window: float, default=np.inf
+            the allowed window for matching the groups (when groups variable is 
+            continuous)  
         orderN: bool, default=False
-            if True then randomly pick O(N) test pairs, if match_groups is True then 
-            the random pairs have the same group labels
-        random_state: float, default=None
-            if specified the function returns the same randomly picked O(N) test
-            pairs, redundant when orderN=False.
-        Returns:
-        --------
-        train and test indices
+            if True then randomly picks O(N) test pairs that satisfy rankable and 
+            group matching conditions
+        closest_match: bool, default=False
+            if True then pairs those test samples for which groups variable is closest
+        random_state: int, default None
+            seed for the random number generator when orderN=True and 
+            match_closest=False.
+
+    Returns:
+    --------
+    train and test indices
         '''
+    def split(self, X, y, erry=10e-4, groups=None, match_window=np.inf, 
+              orderN=False, closest_match=False, random_state=None):
+
         X, y, groups = indexable(X, y, groups)
         num_samples = _num_samples(X)
         if num_samples<2: 
             raise ValueError ('Number of samples must be greater than or equal to 2.') 
-        if groups is None and match_groups is True:
-            raise ValueError ('Argument "groups" should be specified if "match_groups" is True.')
-        if erry is None:
-            erry = np.zeros(num_samples, dtype=float)
-        if type(erry) is float or type(erry) is int:
-            erry = np.full_like(np.arange(num_samples, dtype=float), erry)
-        if type(match_window) is int or type(match_window) is float:
-            match_window = np.full_like(np.arange(num_samples, dtype=float), 
-                                        match_window)
-        return self.train_test_indices(X, y, erry, groups, match_groups, 
-                                       match_window, orderN, random_state)
+        return self.generate_train_test(X, y, erry, groups, match_window,
+                                orderN, closest_match, random_state)
 
-
-    def train_test_indices(self, X, y, erry, groups, match_groups, 
-                            match_window, orderN, random_state):
+    def generate_train_test(self, X, y, erry, groups, match_window, 
+                    orderN, closest_match, random_state):
         num_samples = _num_samples(X)
         indices = np.arange(num_samples)
-        if orderN is False:
+
+        def rankable_match(a, b):
+            '''
+            Determines if the pair of indicies (a, b) are a "rankable" test pair
+            which satisfy the
+            rankable label conditions:
+                y[a]!= y[b] for categorical
+                abs(y[a] - y[b] < max(erry[a], erry[b])) for numerical
+                y[a] = (True, A) and y[b] = (True, B) or
+                y[a] = (True, A) and  y[b] = (False, B) and A<B or
+                y[a] = (False, A) and y[b] = (True, B) and A>B for survival prediction
+            group-matching conditions:
+                groups[a]==groups[b] for categorical groups
+                abs(groups[a]-groups[b]) < match_window for continuous groups
+            '''
+
+            label_str = y.dtype.type is np.str_ and y[a]!=y[b]
+            label_numeric = ((y.dtype.type is np.float_ or y.dtype.type is np.int_) \
+                                and a!=b and abs(y[a] - y[b]) > max(erry[a], erry[b]))
+            try:
+                label_surv = (a!=b and \
+                              (y.dtype[0].type is np.bool_ and \
+                                   (y.dtype[1].type is np.float_ or \
+                                    y.dtype[1].type is np.int_)
+                              ) and \
+                              ((y[a][0]==True and y[b][0]==True) or \
+                               (y[a][0]==True and y[b][0]==False and y[a][1]<y[b][1])or \
+                               (y[a][0]==False and y[b][0]==True and y[a][1]>y[b][1])
+                              )
+                             )
+            except KeyError:
+                label_surv = False
+            
+            groups_str = groups.dtype.type is np.str_ and groups[a]==groups[b]
+            groups_numeric = ((groups.dtype.type is np.int_ or \
+                            groups.dtype.type is np.float_) and \
+                            abs(groups[a]-groups[b]) < match_window)
+            
+            if ((label_str and groups_str) or \
+                (label_str and groups_numeric) or \
+                (label_numeric and groups_str) or \
+                (label_numeric and groups_numeric) or \
+                (label_surv and groups_str) or\
+                (label_surv and groups_numeric)):
+                return True
+            else:
+                return False
+            
+        def closest_rankable_match(a):
+            '''
+            Gets the closest rankable match for the index a for continuous groups
+            rankable label conditions:
+                y[a]!= y[b] for categorical
+                abs(y[a] - y[b] < max(erry[a], erry[b])) for numerical
+                y[a] = (True, A) and y[b] = (True, B) or
+                y[a] = (True, A) and  y[b] = (False, B) and A<B or
+                y[a] = (False, A) and y[b] = (True, B) and A>B for survival prediction
+            closest-match condition:
+                abs(groups[a]-groups[b]) is minimum
+            '''
+            delta = np.inf
+            for b in indices:
+                label_str = y.dtype.type is np.str_ and y[a]!=y[b]
+                label_numeric = ((y.dtype.type is np.float_ or \
+                                  y.dtype.type is np.int_) and \
+                                  a!=b and abs(y[a] - y[b]) > max(erry[a], erry[b]))
+                try:
+                    label_surv = (a!=b and\
+                                   (y.dtype[0].type is np.bool_ and \
+                                        (y.dtype[1].type is np.float_ or \
+                                        y.dtype[1].type is np.int_)
+                                   ) and \
+                                  ((y[a][0]==True and y[b][0]==True) or \
+                                   (y[a][0]==True and y[b][0]==False and y[a][1]<y[b][1])or \
+                                   (y[a][0]==False and y[b][0]==True and y[a][1]>y[b][1])
+                                  )
+                                 )
+                except KeyError:
+                    label_surv = False
+                if (abs(groups[a]-groups[b])< delta and \
+                    (label_str or label_numeric or label_surv)):
+                    index = b
+                    delta = abs(groups[a]-groups[b])
+            return index
+
+        def train_test_indices(a, b):
+            '''
+            Returns the train and test indices as numpy arrays
+            '''
+            test_indices = np.array([a, b])
+            test_mask = np.zeros(num_samples, dtype=bool)
+            test_mask[test_indices] = True
+            train_indices = indices[np.logical_not(test_mask)]
+            return train_indices, test_indices
+
+
+        if erry is None:
+            erry = np.zeros(num_samples, dtype=float)
+
+        if type(erry) is float or type(erry) is int:
+            erry = np.full_like(np.arange(num_samples, dtype=float), erry)
+        
+        if groups is None:
+            groups = np.zeros(num_samples, dtype=int)
+        
+        
+        # O(N^2) pairs
+        if orderN is False and closest_match is False:
             for i, j in combinations(range(num_samples), 2):
-                if match_groups is True:
-                    if (match_window is None and groups[i] != groups[j]):
-                        continue
-                    elif (match_window.any() is not None and \
-                    (abs(groups[i]-groups[j]) > min(match_window[i], match_window[j]))):
-                        continue
-                if abs(y[i] - y[j]) < max(erry[i], erry[j]):
+                if rankable_match(i, j):
+                    yield train_test_indices(i, j)
+                else:
                     continue
-                test_index = np.array([i, j])
-                test_mask = np.zeros(num_samples, dtype=bool)
-                test_mask[test_index] = True
-                train_index = indices[np.logical_not(test_mask)]
-                yield  train_index, test_index
+        # O(N) pairs
         else:
             for i in indices:
-                if match_groups is False:
-                    try:
-                        if random_state is not None:
-                            r = np.random.RandomState(random_state+i)
-                            j = r.choice([k for k in indices if i!=k and \
-                                        abs(y[i]-y[k]) >= max(erry[i], erry[k])])
-                        else:
-                            j = choice([k for k in indices if i!=k and \
-                                abs(y[i]-y[k]) >= max(erry[i], erry[k])])
-                    except ValueError:
-                        continue
-                else:
-                    if match_window is None:
-                        try:
-                            if random_state is not None:
-                                r = np.random.RandomState(random_state+i)
-                                j = r.choice([k for k in indices if i!=k and \
-                                      groups[i] == groups[k] and \
-                                      abs(y[i]-y[k]) >= max(erry[i], erry[k])])
-                            else:
-                                j = choice([k for k in indices if i!=k and \
-                                    groups[i] == groups[k] and \
-                                    abs(y[i]-y[k]) >= max(erry[i], erry[k])])
-                        except ValueError:
-                            continue
+                if closest_match is False:
+                    if random_state is None:
+                        r = RandomState(randint(2**32-1))
                     else:
-                        try:
-                            j = choice([k for k in indices if i!=k and \
-                            abs(groups[i]-groups[k]) <= min(match_window[i], 
-                            match_window[k]) and \
-                            abs(y[i]-y[k]) >= max(erry[i], erry[k])])
-                        except ValueError:
-                            continue
-                
-                test_index = np.array([i,j])
-                test_mask = np.zeros(num_samples, dtype=bool)
-                test_mask[test_index] = True
-                train_index = indices[np.logical_not(test_mask)]
-                yield  train_index, test_index
-
+                        r = RandomState(random_state+i)
+                    try:
+                        j = r.choice([k for k in indices if rankable_match(i, k)])
+                        yield train_test_indices(i, j)
+                    except ValueError:
+                        pass
+                else:
+                    try:
+                        j = closest_rankable_match(i)
+                        yield train_test_indices(i, j)
+                    except UnboundLocalError:
+                        pass
